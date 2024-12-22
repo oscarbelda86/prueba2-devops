3273ae5e6531707ee1d60ac7973353c90cf693d1
oscarbelda86 <oscarbelda86@gmail.com>
2024-12-22 01:16:49 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('¡Hola, mundo desde Node.js!');
+    res.send('¡Hola, he aprendido a usar contenedores!');
 });
 
 app.get('/api', async (req, res) => {
